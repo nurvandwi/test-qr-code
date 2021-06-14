16e452ed2bb237386e151d1ab5640f24e75987d8
nurvandwi <nurvandwi20@gmail.com>
2021-06-14 17:02:14 +0700

    update section voucher

---
+++ src/components/Main-Component/CardVoucher.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card mt-2 p-2" style="max-width: 330px">
+    <div class="row no-gutters">
+      <div class="col-3">
+        <img
+          height="50"
+          src="https://img.icons8.com/dotty/80/000000/starred-ticket.png"
+        />
+      </div>
+      <div class="col-9 align-self-center">
+        <div class="card-body p-0">
+          <h5
+            class="card-title"
+            style="white-space: nowrap; font-size: 15px; margin: 0"
+          >
+            {{ title }}
+          </h5>
+          <p class="card-text">
+            <small class="text-muted"> {{ subtitle }}</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "subtitle"],
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/VoucherAvailable.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <Header :title="'Voucher Cashback'" />
+    <div class="mb-custom">
+      <CardVoucher
+        :title="'Voucher Cashback @Rp.100.000'"
+        :subtitle="'Periode: Januari 2021'"
+      />
+      <CardVoucher
+        :title="'Voucher Cashback @Rp.50.000'"
+        :subtitle="'Periode: Januari 2021'"
+      />
+      <CardVoucher
+        :title="'Voucher Cashback @Rp.25.000'"
+        :subtitle="'Periode: Januari 2021'"
+      />
+      <CardVoucher
+        :title="'Voucher Cashback @Rp.100.000'"
+        :subtitle="'Periode: Januari 2021'"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Catalog/Header.vue";
+import CardVoucher from "../components/Main-Component/CardVoucher.vue";
+export default {
+  components: { CardVoucher, Header },
+};
+</script>
+
+<style scoped>
+.mb-custom {
+  margin-top: 5rem;
+}
+/deep/ .backtheme {
+  background-color: #b30000;
+}
+</style>
